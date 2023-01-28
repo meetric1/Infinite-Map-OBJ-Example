564196b53f66cf93855c9ac4c9f535c1f572ef55
Mee12345 <73554599+Mee12345@users.noreply.github.com>
2023-01-28 18:38:19 -0500

    Initial Creation

---
+++ object test/lua/infmap/gm_infmap_objexample/loadobject.lua
@@ -0,0 +1,22 @@
+
+// Clear pervious data if it exists
+InfMap.clear_parsed_objects()
+
+// Create a matrix to scale the model
+local mat = Matrix()
+mat:SetTranslation(Vector(0, 0, 0))		// Sets the models position
+mat:SetAngles(Angle(0, 0, 0))			// Sets the models rotation
+mat:SetScale(Vector(1, 1, 1) * 39.3701)	// Conversion from Meters to Inches
+
+// visuals = 1
+// collision = 2
+
+// nocull Shader = No backface culling
+// alphatest Shader = Enable transparency on the model
+
+// Create our visual model, note the "1" which means it is ONLY visual
+InfMap.parse_obj("cloud", mat, 1, "nocull alphatest")
+
+// Create our physical model, note the "2" which means it is ONLY collision
+// Because it is collision, we do not need the shader option
+InfMap.parse_obj("cloud_collision", mat, 2)
